31be379f3426015b4eacf235a174a7eb9b996b96
Wesdell <wesdell17@gmail.com>
2024-06-22 18:55:15 -0500

    Fix code typo in hierarchy

--- src/TrueFalseQuestion.java
+++ src/TrueFalseQuestion.java
@@ -1,9 +1,9 @@
-public class TrueFalseQuestion {
+public class TrueFalseQuestion extends Question {
   public TrueFalseQuestion(String statement, Selection selection) {
     super(statement, parseToOptions(selection));
   }
 
-  public static Options[] parseToOptions(Selection selection) {
+  public static Option[] parseToOptions(Selection selection) {
     Option[] options = new Option[selection.getClass().getEnumConstants().length];
     int i = 0;
     for (Selection option : selection.getClass().getEnumConstants()) {
