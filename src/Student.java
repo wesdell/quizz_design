95b31dbeaceb2813a81667d4d23a254763b15233
Wesdell <wesdell17@gmail.com>
2024-06-22 18:41:03 -0500

    Set Student class attributes and methods

---
+++ src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+  private final String name;
+  private Quiz quiz;
+  private double grade;
+
+  public Student(String name) {
+    this.name = name;
+  }
+
+  public void setQuiz(Quiz quiz) {
+    this.quiz = quiz;
+  }
+
+  public void answer(int... answers) {
+    grade = quiz.getGrade(answers);
+  }
+  
+  public String printGrade() {
+    return name + " obtuvo la calificacion de " + grade + "/" + quiz.getQuizPoints() + " en " + quiz;
+  }
+}
