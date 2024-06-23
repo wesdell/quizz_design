809e912132c65cdacf442d7a5537173d6fd602ed
Wesdell <wesdell17@gmail.com>
2024-06-22 20:26:08 -0500

    Validate one correct option per question

--- src/Question.java
+++ src/Question.java
@@ -6,11 +6,24 @@ public abstract class Question {
   public Question(Statement statement, Option... options) {
     this.statement = statement;
     this.options = options;
+    checkOneCorrectOption(options);
     score = 1;
   }
 
   public double grade(int answer) {
     return (options[answer - 1] instanceof CorrectOption) ? score : 0;
+  }
+
+  private void checkOneCorrectOption(Option[] options) {
+    int correctOptionsCounter = 0;
+    for (Option option : options) {
+      if (option instanceof CorrectOption) {
+        correctOptionsCounter += 1;
+      }
+    }
+    if (correctOptionsCounter != 1) {
+      throw new Error();
+    }
   }
 
   public Statement getStatement() {
